71f2e96c15653d69970aacbb77ddc8615149ced6
Martin Schweizer <martin.schweizer@bd.so.ch>
2023-10-18 00:27:26 +0200

    [CO2-Messung]First commit

--- CO2-Messung/main.py
+++ CO2-Messung/main.py
@@ -12,30 +12,30 @@ display.set_backlight(0.5)
 # Init LED
 led = RGBLED(6, 7, 8) #RGBLED PIN definition
 
-# Fonction qui retourne le niveau de CO2 et TVOC
+# CO2 und TVOC
 def mesure_capteur():
     air_quality = sgp30.get_air_quality()
     eCO2 = air_quality[BreakoutSGP30.ECO2]
     TVOC = air_quality[BreakoutSGP30.TVOC]
     return eCO2, TVOC
   
-# Fonction qui efface l'ecran
+# Bildschirm leeren
 def clear():
     my_pen = display.create_pen(0,0,0)
     display.set_pen(my_pen)
     display.clear()
     display.update()
       
-# Gere la couleur en fonction du niveau de CO2
+# Farbe festlegen anhand des CO2-Niveaus
 def get_color(CO2):
     if eCO2 < 500:
-        return 0,0xff,0 #vert
+        return 0,0xff,0 #grün
     elif eCO2 < 1000:
         return 0xff,165,0 #orange
     else:
-        return 0xff,0,0 #rouge
+        return 0xff,0,0 #rot
 
-# Initialisation du capteur
+# Sensor Initialisieren
 PINS_PICO_EXPLORER = {"sda": 20, "scl": 21}
 i2c = PimoroniI2C(**PINS_PICO_EXPLORER)
 sgp30 = BreakoutSGP30(i2c)
@@ -43,19 +43,25 @@ sgp30.soft_reset()
 utime.sleep(1)    
 sgp30.start_measurement(False)
   
-# Boucle du programme principal
-while True:
-    # Lecture de la qualite de l'air sur le capteur    
+# Hauptprogramm
+while True:  
+    #Messung vornehmen 
     eCO2, TVOC = mesure_capteur()       
     print("CO2 ", eCO2, " TVOC ", TVOC)
-    r,g,b = get_color(eCO2)
-    # Affichage sur l'ecran
+
+    #Werte auf dem Bildschirm ausgeben
+    #Statischer Text
     my_pen = display.create_pen(0xff,0xff,0xff)
     display.set_pen(my_pen)      
     display.text("CO2 ppm", 30, 10, 240, 4)
+
+    #CO2-Wert
+    r,g,b = get_color(eCO2)
     my_pen = display.create_pen(r,g,b)
     display.set_pen(my_pen)      
     display.text("{}".format(eCO2), 40, 70, 240, 5)
+
+    #LED-Farbe ändern
     led.set_rgb(r,g,b)
     display.update()
     utime.sleep(1)                                    
